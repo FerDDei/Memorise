4a50c78f7bc0da887fe9a92d7ffa18e20eea0b4d
Frederic Allen-Stevens <freddiejohnallenstevens@gmail.com>
2023-11-18 13:02:48 +0000

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Freddie Allen-Stevens on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
