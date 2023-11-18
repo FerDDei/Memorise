a8d8f0930eb3952a71ebb362498b3147132c33b4
Frederic Allen-Stevens <freddiejohnallenstevens@gmail.com>
2023-11-18 13:52:16 +0000

    Second commit test (Multiple before push)

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Test")
+            Text("Test 2")
         }
         .padding()
     }
